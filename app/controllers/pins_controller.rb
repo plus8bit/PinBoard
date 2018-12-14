b7fc8525c0519e97d5a24d56faa4181949e66ac2
Nick <plus0bit@gmail.com>
2018-12-14 18:25:15 +0300

    Add new, create, pin_params actions

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,4 +1,19 @@
 class PinsController < ApplicationController
   def index
   end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def CreatePins
+    @pin = Pin.new(pin_params)
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :description)
+  end
+
 end
