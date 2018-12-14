765e79903207806730ceefd9fab03cebcf355c15
Nick <plus0bit@gmail.com>
2018-12-14 21:21:43 +0300

    Add show view, display all pins on index page

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,12 @@
 class PinsController < ApplicationController
+
+  before_action :find_pin, only: [:show, :edit, :update, :destroy] 
+
   def index
+    @pins = Pin.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -16,10 +23,15 @@ class PinsController < ApplicationController
     end
   end
 
+
   private
 
   def pin_params
     params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin
+    @pin = Pin.find(params[:id])
   end
 
 end
