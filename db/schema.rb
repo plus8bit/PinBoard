6f2478eff2e4aea5510d834100ff9d0e04328394
Nick <plus0bit@gmail.com>
2018-12-15 14:03:33 +0300

    Add  associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_190634) do
+ActiveRecord::Schema.define(version: 2018_12_15_105547) do
 
   create_table "pins", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
